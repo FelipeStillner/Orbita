6e0fc04d51118e545f360bb05637e364f4f1a715
Felipe Stillner Eufranio <felipeeufranio@MacBook-Air-de-Felipe.local>
2025-12-21 22:39:40 +0100

    feat: improve places query

--- internal/adapter/osm/fetch_places.go
+++ internal/adapter/osm/fetch_places.go
@@ -26,24 +26,22 @@ func (c *Client) FetchPlaces(lat, long float64, radiusMeters int) (*OSMResponse,
 const overpassURL = "https://overpass-api.de/api/interpreter"
 
 const templateQuery = `
-			[out:json][timeout:25];
-			(
-			  // --- FOOD (1km Radius) ---
-			  // Only "Restaurants". We removed Cafes and Bars to reduce noise.
-			  node["amenity"="restaurant"]["name"](around:2000,%[1]f,%[2]f);
+	[out:json][timeout:25];
+	(
+	  // --- TOURISM & ARTS ---
+	  nwr["tourism"~"museum|gallery|viewpoint|attraction|zoo|theme_park|aquarium"]["wikidata"](around:10000,%[1]f,%[2]f);
 
-			  // --- MAJOR TOURISM (5km Radius + Wikipedia Required) ---
-			  // We removed "viewpoint", "attraction", "memorial", "artwork".
-			  // We only keep buildings that people pay to enter.
-			  node["tourism"~"museum|zoo|theme_park|aquarium"]["wikipedia"](around:10000,%[1]f,%[2]f);
+	  // --- PERFORMING ARTS (Operas, Theaters) ---
+	  nwr["amenity"~"theatre|arts_centre|planetarium"]["wikidata"](around:10000,%[1]f,%[2]f);
 
-			  // --- HISTORY (5km Radius + Wikipedia Required) ---
-			  // Only physical structures.
-			  node["historic"~"castle|ruins|fort|archaeological_site"]["wikipedia"](around:10000,%[1]f,%[2]f);
+	  // --- HISTORY & MONUMENTS ---
+	  nwr["historic"]["wikidata"](around:10000,%[1]f,%[2]f);
 
-			  // --- RELIGION (5km Radius + Wikipedia Required) ---
-			  // Only Cathedrals (ignore small chapels)
-			  node["building"="cathedral"]["wikipedia"](around:10000,%[1]f,%[2]f);
-			);
-			out body;
-		`
+	  // --- RELIGION (Cathedrals) ---
+	  nwr["building"="cathedral"](around:10000,%[1]f,%[2]f);
+
+	  // --- PUBLIC SPACES (Squares/Plazas) ---
+	  nwr["place"="square"]["wikidata"](around:10000,%[1]f,%[2]f);
+	);
+	out center;
+`
