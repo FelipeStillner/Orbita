c62f8e1cd93f5eb5d81658c0da32a6afe099ff63
Felipe Stillner Eufranio <felipeeufranio@Apollo.local>
2026-04-01 11:44:00 +0200

    test: print env

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -6,6 +6,9 @@ import { router } from "./app/Router";
 import "./index.css";
 import { AuthProvider } from "@context/AuthContext";
 
+console.log("VITE_GOOGLE_CLIENT_ID:", import.meta.env.VITE_GOOGLE_CLIENT_ID);
+console.log("VITE_STADIA_API_KEY:", import.meta.env.VITE_STADIA_API_KEY);
+
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
